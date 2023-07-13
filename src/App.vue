b8d2602697c3cfe0774b9339d59c8a0304840111
newgentechnicon <41534520+newgenerr@users.noreply.github.com>
2023-07-13 09:49:35 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<!-- For feature/profile Branches -->
+<!-- For feature/EmployeeManagement Branches -->
 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
@@ -10,7 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it yeah!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
